1968b4383d133d536eca9554e3ae502df4f0b5eb
Anahid <anahidhemmat2130@gmail.com>
2022-04-27 14:30:27 +0400

    outputting lists

--- Ninja-tutorial/dojo-blog/src/Home.js
+++ Ninja-tutorial/dojo-blog/src/Home.js
@@ -1,33 +1,26 @@
 import { useState } from "react";
 
 const Home = () => {
-  // let name = "mario";
   //reactive values
-  const [name, setName] = useState("mario");
-  const [age, setAge] = useState(25);
-  const handleClick = () => {
-    setName("luigi");
-    setAge(30);
-  };
-
-  // const handleClickAgain = (name, e) => {
-  //   console.log("Hello " + name, e.target);
-  // };
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
 
   return (
     <div className="home">
-      <h2>Home Page</h2>
-      <p>
-        {name} is {age} years old.
-      </p>
-      <button onClick={handleClick}>Click me</button>
-      {/* <button
-        onClick={(e) => {
-          handleClickAgain("Nana", e);
-        }}
-      >
-        Click me again
-      </button> */}
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
