d4847c88d3331dea3c614f759140c65f02394708
Anahid <anahidhemmat2130@gmail.com>
2022-04-27 13:10:59 +0400

    click events

--- Ninja-tutorial/dojo-blog/src/Home.js
+++ Ninja-tutorial/dojo-blog/src/Home.js
@@ -1,7 +1,22 @@
 const Home = () => {
+  const handleClick = () => {
+    console.log("HELLO");
+  };
+
+  const handleClickAgain = (name, e) => {
+    console.log("Hello " + name, e.target);
+  };
   return (
     <div className="home">
       <h2>Home Page</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button
+        onClick={(e) => {
+          handleClickAgain("Nana", e);
+        }}
+      >
+        Click me again
+      </button>
     </div>
   );
 };
