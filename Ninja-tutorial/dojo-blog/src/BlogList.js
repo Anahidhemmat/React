b74acb7694ba401ed823e6f3947dcee69fad41f7
Anahid <anahidhemmat2130@gmail.com>
2022-04-27 15:07:28 +0400

    Reusing Components

--- Ninja-tutorial/dojo-blog/src/BlogList.js
+++ Ninja-tutorial/dojo-blog/src/BlogList.js
@@ -2,15 +2,9 @@ const BlogList = ({ blogs, title }) => {
   //   const blogs = props.blogs;
   return (
     <div className="blog-list">
+      <h2>{title}</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
-          <h2
-            style={{
-              color: "black",
-            }}
-          >
-            {title}
-          </h2>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
         </div>
