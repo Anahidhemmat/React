59e5c35dad88d465f503762325c41945c449fcc6
Anahid <anahidhemmat2130@gmail.com>
2022-04-27 14:53:41 +0400

    props

---
+++ Ninja-tutorial/dojo-blog/src/BlogList.js
@@ -0,0 +1,22 @@
+const BlogList = ({ blogs, title }) => {
+  //   const blogs = props.blogs;
+  return (
+    <div className="blog-list">
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2
+            style={{
+              color: "black",
+            }}
+          >
+            {title}
+          </h2>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BlogList;
