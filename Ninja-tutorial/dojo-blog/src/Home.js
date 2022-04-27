5442d54885e7a1c3a7c4afc52c3c7a8c1721b414
Anahid <anahidhemmat2130@gmail.com>
2022-04-28 00:00:01 +0400

    using json server

--- Ninja-tutorial/dojo-blog/src/Home.js
+++ Ninja-tutorial/dojo-blog/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -14,6 +14,10 @@ const Home = () => {
     },
   ]);
 
+  useEffect(() => {
+    console.log("use effect ran");
+    console.log(blogs);
+  });
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
