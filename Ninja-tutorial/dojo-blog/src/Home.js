4a6c8d4a70c924e2291051bcacce94ce9b25a337
Anahid <anahidhemmat2130@gmail.com>
2022-05-08 15:25:39 +0400

    fetching data with useEffect

--- Ninja-tutorial/dojo-blog/src/Home.js
+++ Ninja-tutorial/dojo-blog/src/Home.js
@@ -3,29 +3,32 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   //reactive values
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
   useEffect(() => {
-    console.log("use effect ran");
-    console.log(blogs);
-  });
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
+    fetch("http://localhost:8001/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        return setBlogs(data);
+      });
+  }, []);
+
+  // const handleDelete = (id) => {
+  //   const newBlogs = blogs.filter((blog) => blog.id !== id);
+  //   setBlogs(newBlogs);
+  // };
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+      {blogs && (
+        <BlogList
+          blogs={blogs}
+          title="All Blogs!"
+          // handleDelete={handleDelete}
+        />
+      )}
       {/* <BlogList
         blogs={blogs.filter((blog) => blog.author === "mario")}
         title="Mario's Blogs!"
