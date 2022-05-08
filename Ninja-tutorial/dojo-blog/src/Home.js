9349aec1f5d4662d9c0ab48ea4fb562cdabb1a72
Anahid <anahidhemmat2130@gmail.com>
2022-05-08 23:44:33 +0400

    making costum hook

--- Ninja-tutorial/dojo-blog/src/Home.js
+++ Ninja-tutorial/dojo-blog/src/Home.js
@@ -1,30 +1,13 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  //reactive values
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetch("http://localhost:8001/blogs")
-      .then((res) => {
-        if (!res.ok) {
-          throw Error("could not fetch the data");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-        setIsPending(false);
-        setError(null);
-      })
-      .catch((err) => {
-        setError(err.message);
-        setIsPending(false);
-      });
-  }, []);
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8001/blogs");
 
   // const handleDelete = (id) => {
   //   const newBlogs = blogs.filter((blog) => blog.id !== id);
